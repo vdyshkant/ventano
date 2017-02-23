fbe04ef3832881f128826c1ae02f24b2735e4d41
vdyshkant <yong.knox@yandex.ru>
2017-02-23 23:06:26 +0300

    minor edits

---
+++ src/js/modules/004_features.js
@@ -0,0 +1,74 @@
+// features-expand
+
+$( document ).ready(function() {
+
+	if($('body').is('.page-index')){
+
+		if ($(".page-index .features .js-more").length) {
+				// $(".js-more[data-moreNum='2']").click(function() {
+				// 	$(this).parent('.js-feature-item').toggleClass('active');
+				// });
+
+				expandFeaturesItem(1);
+				expandFeaturesItem(2);
+				expandFeaturesItem(3);
+				expandFeaturesItem(4);
+				expandFeaturesItem(5);
+				expandFeaturesItem(6);
+				expandFeaturesItem(7);
+		}
+
+	}
+
+	function expandFeaturesItem(identifier){
+		$('.js-more[data-more-num=' + identifier + ']').click(function(e) {
+			e.preventDefault();
+			// $(this).css( "color", "red" );
+			if ($(this).parents('.features__item-container').is('.active')) {
+				$('.js-feature-item[data-more-container="' + identifier + '"]').removeClass('active');
+				$('.js-extra[data-more-option="' + identifier + '"]').slideUp(300);
+
+				$('.features-smoothy[data-more-smoothy="' + identifier + '"] img:nth-child(1)').fadeOut(300);
+				$('.features-smoothy[data-more-smoothy="' + identifier + '"] img:nth-child(2)').fadeIn(300);
+			} else {
+				$('.js-feature-item').removeClass('active');
+				$('.js-extra').slideUp(300);
+								$('.features-smoothy img:nth-child(1)').fadeOut(300);
+								$('.features-smoothy img:nth-child(2)').fadeIn(300);
+
+
+				$('.js-feature-item[data-more-container="' + identifier + '"]').toggleClass('active');
+				$('.js-extra[data-more-option="' + identifier + '"]').slideToggle(300);
+
+
+				$('.features-smoothy[data-more-smoothy="' + identifier + '"] img:nth-child(1)').fadeToggle(300);
+				$('.features-smoothy[data-more-smoothy="' + identifier + '"] img:nth-child(2)').fadeToggle(300);
+			}
+
+		});
+	}
+
+}); // eof .ready
+
+// features-expand end
+
+//
+//
+//
+// data-id:"+$(identifier).data('id')+"
+//
+//
+// function expandFeaturesItem(identifier){
+// 	$(".js-more[data-moreNum='+" identifier "+']").click(function() {
+// 		$(this).parent('.js-feature-item[data-moreContainer="' + identifier + '"]').toggleClass('active');
+// 	});
+// }
+//
+// // Псевдокод:
+//
+// парсим кнопку, которую жмет пользователь, вместе с дата-идентификатором
+//
+// исходя и з того, с каким дата-идентификатором нажата кнопка,
+// по значению переменной этого id обращаемся к необходимому нам блоку и добавляем ему класс .active
+// выключение по тому же принципу либо через toggle
+// описать при каких услових закрывать менюшку
---
+++ src/js/modules/005_sale-slider.js
@@ -0,0 +1,53 @@
+// slider
+
+// $( document ).ready(function() {
+$(window).load(function() {
+
+	if($('body').is('.page-index')){
+		$("ul#slider1").bxSlider({
+        // http://stackoverflow.com/a/20342392
+        // BxSlider displayes last slide as first slide | dublicating ids
+
+		    speed: 300,
+		    pager: true,
+		    nextText: '',
+		    prevText: '',
+				prevSelector: ('.slider-sales-controls__left'),
+				nextSelector: ('.slider-sales-controls__right'),
+
+        // looping
+        auto: true,
+        infiniteLoop: true,
+        pause: 5000
+		    //  onSlideBefore:function($slideElement, oldIndex, newIndex){
+		    //    changeRealThumb(realThumbSlider,newIndex);
+		    //
+		    //  }
+
+		});
+
+    // // get the children behind
+    // var $prependedChildren = getArraySample($children, 0, options.moveSlideQty, 'backward');
+    //
+    // // add each prepended child to the back of the original element
+    // $.each($prependedChildren, function(index) {
+    //     $parent.prepend($(this));
+    // });
+	}
+
+
+	$('.slider-sales .range').mouseenter(function() {
+		$('.slider-sales-controls').fadeIn(300);
+
+			// console.info('.nextend-arrow).fade IN;');
+
+	}).mouseleave(function() {
+			$('.slider-sales-controls').fadeOut(300);
+
+			// console.info('.nextend-arrow).fade OUT;');
+
+	});
+
+}); // eof .ready
+
+// slider end
