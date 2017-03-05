6b1be675365771d905f7b5786d75bf65208f1901
vdyshkant <yong.knox@yandex.ru>
2017-03-05 22:35:56 +0300

    index::window -- made beta ver., minor edits (del unnec. styles)

--- src/js/modules/common.js
+++ src/js/modules/common.js
@@ -945,3 +945,41 @@ function hideMainOverlay() {
 // });
 
 // external js: masonry.pkgd.js EOF
+
+
+/* change window text
+	================================================ */
+
+(function($) {
+
+ if($('body').is('.page-index')){
+
+  //  new machMedia resizer; the fastest one
+  //  change text on resize event.
+  var mq  = window.matchMedia( '(max-width: 1159px)' );
+  mq.addListener(function(mq) {
+     if (mq.matches) {
+       change2newString();
+     } else {
+       change2originString();
+     }
+  });
+
+  // instant change text on mobile-tablet hw.
+  if ($(window).width() < 1160) {
+      change2newString()
+  }
+
+
+ }
+})(jQuery); // eof autoFunction
+
+function change2newString(){
+  var string = 'Официальный партнёр';
+  $('.window-esscence-overtitle-text').text(string);
+}
+function change2originString(){
+  var origin = 'Авторизированный партнёр';
+  $('.window-esscence-overtitle-text').text(origin);
+}
+/* eof ^^^ */
