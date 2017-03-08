adeddb10c4a7214be8dfb278bd1ca3b8d9090f9f
vdyshkant <yong.knox@yandex.ru>
2017-03-08 08:30:24 +0300

    index::modal-content -- made beta ver. plus adaptation

--- src/js/modules/common.js
+++ src/js/modules/common.js
@@ -1053,3 +1053,49 @@ $(document).ready(function() {
 }); // eof ready
 
 // tabs for page-windsystem::choose module END
+
+
+/* .modal-content.modal-home
+  ================================================ */
+(function($) {
+  if($('body').is('.page-index')){
+
+    showMainModal();
+
+    closeMainModal();
+
+    hideMainOverlay();
+
+
+    $('html body').on('keyup', function(e) {
+    	if (e.keyCode === 27) {
+    			$('.modal-content').fadeOut(300);
+    			$('.modal-home-overlay').fadeOut(300);
+    	}
+    });
+
+  }
+})(jQuery); // eof autoFunction
+
+function showMainModal() {
+    $('#invoke_modal').click(function() {
+        $('.modal-content').fadeIn(300);
+        $('.modal-home-overlay').fadeIn(300);
+    });
+}
+
+function closeMainModal() {
+    $('.modal-content-close').click(function() {
+        $('.modal-content').fadeOut(300);
+        $('.modal-home-overlay').fadeOut(300);
+    });
+}
+
+function hideMainOverlay() {
+    $('.modal-home-overlay').click(function() {
+        $('.modal-content').fadeOut(300);
+        $('.modal-home-overlay').fadeOut(300);
+    });
+}
+
+/* eof ^^^ */
