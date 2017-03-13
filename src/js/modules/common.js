053789373a11caa0666fea1843be213109b2cb7c
vdyshkant <yong.knox@yandex.ru>
2017-03-13 15:13:29 +0300

    index::headhesive(floating burger) finished beta ver., debuged

--- src/js/modules/common.js
+++ src/js/modules/common.js
@@ -1108,11 +1108,20 @@ function hideMainOverlay() {
  if($('body').is('.page-index')){
 
 	 // burger animation itself:
-	 $('.floating-burger-btn #click').click(function() {
-			 $('.floating-burger-btn').toggleClass("burger-active");
+	 $('.banner--clone .floating-burger-btn #click').click(function() {
+			 $('.banner--clone .floating-burger-btn').toggleClass("burger-active");
 			 $('.floating-burger-overlay').fadeToggle(200, 'linear');
 			//  $('.floating-burger-essence').slideToggle(200, 'easeInOutCubic');
-			 $('.floating-burger-essence').toggleClass('opened');
+			 $('.banner--clone .floating-burger-essence').toggleClass('opened');
+	 });
+
+   $('.floating-burger-overlay').click(function() {
+			$('.banner--clone .floating-burger-btn').removeClass("burger-active");
+      $('.banner--clone .floating-burger-btn #click').attr('checked', false);
+      if ((window.matchMedia("(max-width: 1159px)").matches)) {
+        $('.floating-burger-overlay').fadeOut(200, 'linear');
+      }
+			$('.banner--clone .floating-burger-essence').removeClass('opened');
 	 });
 
 	//  $('.request-callback-nav').click(function() {
